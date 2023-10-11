e543e297b00e85f19c9f236a1789c3bd17a2e9d4
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-10-10 21:52:19 -0500

    feat: :sparkles: Mapping files Dtos in MappingProfiles from Profiles Folder
    @Gonther1

---
+++ API/Profiles/MappingProfiles.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Profiles;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<Pais,PaisDto>()
+        .ReverseMap();
+
+        CreateMap<Departamento,DepartamentoDto>()
+        .ReverseMap();
+
+        CreateMap<Ciudad,CiudadDto>()
+        .ReverseMap();
+
+        CreateMap<Cita,CitaDto>()
+        .ReverseMap();
+
+        CreateMap<Cliente,ClienteDto>()
+        .ReverseMap();
+
+        CreateMap<ClienteTelefono,ClienteTelefonoDto>()
+        .ReverseMap();
+        
+        CreateMap<Raza,RazaDto>()
+        .ReverseMap();
+
+        CreateMap<Mascota,MascotaDto>()
+        .ReverseMap();
+    }
+}
