ebbf1a97c785ffe058bcccdb01e327635b85ad39
Gonther1 <luisandresalvarezsilva@gmail.com>
2023-10-16 21:12:19 -0500

    feat: :sparkles: Create Controllers for all entities
    Create and configurate controller structure basic
    
    @Gonther1

---
+++ API/Controllers/CitaController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers;
+
+public class CitaController : BaseControllerApi
+{
+    
+}
